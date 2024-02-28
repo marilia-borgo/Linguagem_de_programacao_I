6dbe2f6e21ce75751fedcf4faec485bb7330964f
marilia-borgo <marilia.bm01@gmail.com>
2024-02-28 19:21:39 -0300

    programinhas primeira aula

---
+++ MySecondJavaProgram.java
@@ -0,0 +1,15 @@
+public class MySecondJavaProgram {
+    public static void main(String[] args) {
+        int gastoJaneiro = 15000;
+        int gastoFevereiro = 23000;
+        int gastoMarco = 17000;
+
+        int gastoTotalTrimestre = gastoJaneiro + gastoFevereiro + gastoMarco;
+
+        double mediaMensal = gastoTotalTrimestre / 3.0;
+
+        // Imprime os resultados
+        System.out.println("Gasto trimestre: " + gastoTotalTrimestre + " reais");
+        System.out.println("Média trimestre: " + mediaMensal + " reais");
+    }
+}
---
+++ MyThirdJavaProgram.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MyThirdJavaProgram {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("10P1: ");
+        double p1 = scanner.nextDouble();
+        System.out.print("E1: ");
+        double e1 = scanner.nextDouble();
+        System.out.print("E2: ");
+        double e2 = scanner.nextDouble();
+        System.out.print("API: ");
+        double api = scanner.nextDouble();
+        System.out.print("SUB: ");
+        double sub = scanner.nextDouble();
+
+
+        double notaFinal = 0.0;
+        double primeiraParte = (p1 * 0.6 + ((e1 + e2) / 2) * 0.4) * 0.5;
+        double segundaParte = Math.max(((p1 * 0.6 + ((e1 + e2) / 2) * 0.4) - 5.9), 0) / ((p1 * 0.6 + ((e1 + e2) / 2) * 0.4) - 5.9);
+        notaFinal = primeiraParte + segundaParte * api + sub * 0.2;
+
+        System.out.println("A nota final é: " + notaFinal);
+    }
+}
